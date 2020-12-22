fe490b941b10a7ced74cc0bb478bd25eba8be6a6
rattikan631 <69663361+rattikan631@users.noreply.github.com>
2020-12-22 11:20:37 +0700

    Add files via upload

---
+++ week6/java101.js
@@ -0,0 +1,30 @@
+function myFunction3(){
+    let x = document.getElementById("myBtn3").value;
+    //document.getElementById("images").src="green.jpg";
+    document.getElementById("images").src=x;
+}
+function myFunction4(){
+    let x = document.getElementById("myBtn4").value;
+    //document.getElementById("images").src="green.jpg";
+    document.getElementById("images").src=x;
+}
+function myFunction5(){
+    let x = document.getElementById("myBtn5").value;
+    //document.getElementById("images").src="green.jpg";
+    document.getElementById("images").src=x;
+}
+function Function6(){
+    document.getElementById("text").style.color="green";
+}
+function Function7(){
+    document.getElementById("text").style.color="blue";
+}
+function Function8(){
+    document.getElementById("text").style.color="brown";
+}
+function Function9(){
+    document.getElementById("hideDisplay").style.display="none";
+}
+function Function10(){
+    document.getElementById("hideDisplay").style.display="block";
+}
